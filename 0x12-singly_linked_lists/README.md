dd6cbf96b7383fbbfe5a2535983dba4a6b1e0831
Benyarko1 <benyarko.ahmed@gmail.com>
2023-11-16 22:44:57 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,1 +1,7 @@
-Find my readme file
+0x12. C - Singly linked lists
+
+Task 0. Print list
+Task 1. List length
+Task 2. Add node
+Task 3. Add node at the end
+Task 4. Free list
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,27 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LINKED
+#define LINKED
 
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * struct lists_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: legnth of string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char* str;
+	unsigned int len;
+	struct list_s *next;
+}
+	list_t;
 
 	size_t print_list(const list_t *h);
 	size_t list_len(const list_t *h);
-#endif
+
+#endif /*LINKED*/
